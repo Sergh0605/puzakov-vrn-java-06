2519633d1e115b882d137710eb905ecb2a119131
Sergh0605 <puzakovsergei@gmail.com>
2021-10-22 00:41:59 +0300

    Done

--- src/main/java/ru/dataart/academy/java/Calculator.java
+++ src/main/java/ru/dataart/academy/java/Calculator.java
@@ -5,7 +5,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 import java.util.zip.ZipInputStream;
 
 public class Calculator {
@@ -36,27 +35,25 @@ public class Calculator {
      */
 
     public Integer getMaxWordLength(String zipFilePath) {
-        int wordMaxLength = 0;
+        int maxWordLength = 0;
         try (FileInputStream fis = new FileInputStream(zipFilePath);
              BufferedInputStream bis = new BufferedInputStream(fis);
              ZipInputStream zis = new ZipInputStream(bis);
              InputStreamReader isr = new InputStreamReader(zis, StandardCharsets.UTF_8);
              BufferedReader br = new BufferedReader(isr)) {
             while (zis.getNextEntry() != null) {
-                int entryWordMaxLength = br.lines()
-                        .mapToInt(s -> stringsToStreamOfWords(s).mapToInt(String::length).max().orElse(0))
-                        .max().orElse(0);
-                wordMaxLength = Math.max(entryWordMaxLength, wordMaxLength);
+                int entryMaxWordLength = br.lines().mapToInt(this::getMaxLengthOfWordInString).max().orElse(0);
+                maxWordLength = Math.max(entryMaxWordLength, maxWordLength);
             }
         } catch (IOException e) {
             System.out.println("File not found or read-protected.");
         }
-        return wordMaxLength;
+        return maxWordLength;
     }
 
-    private Stream<String> stringsToStreamOfWords(String stringOfWords) {
+    private int getMaxLengthOfWordInString(String stringOfWords) {
         List<String> outputList = new ArrayList<>(Arrays.asList(stringOfWords.split("\\s+")));
-        return outputList.stream();
+        return outputList.stream().mapToInt(String::length).max().orElse(0);
     }
 
 }
